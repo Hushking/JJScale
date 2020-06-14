b0e8350b7f57fa983670d8851e9fddd124238d17
Lucas Torres <lucastorres_candido@hotmail.com>
2020-06-14 19:13:59 -0300

    Ajuste proposta

--- src/models/ProposalModel.js
+++ src/models/ProposalModel.js
@@ -1,14 +1,16 @@
 class ProposalModel{
-    constructor(
+    constructor({
         idproposta
         , idprojeto
         , idaprovador
         , observacao
-    ){
+        , nome
+    }){
         this.idproposta = idproposta
         this.idprojeto = idprojeto
         this.idaprovador = idaprovador
         this.observacao = observacao
+        this.nome = nome
     }
 }
 module.exports = ProposalModel
